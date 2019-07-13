fbee0920231dc3d0fed444c008222c0f16e4981f
Naiks10 <Ultravirus661@gmail.com>
2019-07-13 13:21:14 +0300

    New

--- handler.go
+++ handler.go
@@ -22,21 +22,25 @@ func (m Handler) Start(template string) {
 func (m Handler) Execute(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./templates/"+m.Template+".html", "./templates/footer.html", "./templates/header.html")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("UniversalHandler status : Error => ", err)
+	} else {
+		fmt.Println("UniversalHandler status : OK")
 	}
 	if r.URL.Path != "/"+m.Template {
-		errorHandler(w, r, http.StatusNotFound)
+		ErrorHandler(w, r, http.StatusNotFound)
 		return
 	}
 	t.ExecuteTemplate(w, m.Template, nil)
 }
 
-func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+func ErrorHandler(w http.ResponseWriter, r *http.Request, status int) {
 	w.WriteHeader(status)
 	if status == http.StatusNotFound {
 		t, err := template.ParseFiles("./templates/404.html", "./templates/header.html", "./templates/footer.html")
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("ErrorHandler status : Error => ", err)
+		} else {
+			fmt.Println("ErrorHandler status : OK")
 		}
 		t.ExecuteTemplate(w, "404", nil)
 	}
--- main.go
+++ main.go
@@ -10,18 +10,26 @@ import (
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./templates/index.html", "./templates/footer.html", "./templates/header.html")
 	if err != nil {
+		fmt.Println("IndexHandler status : Error => ", err)
+	} else {
+		fmt.Println("IndexHandler status : OK")
 	}
 	if r.URL.Path != "/" {
-		errorHandler(w, r, http.StatusNotFound)
+		ErrorHandler(w, r, http.StatusNotFound)
 		return
 	}
 	t.ExecuteTemplate(w, "index", nil)
 }
 
 func main() {
-	fmt.Println("listening default port (:3002)")
+	fmt.Println("listening default port")
+
 	port := os.Getenv("PORT")
 	var a int
+	defer func() {
+		fmt.Println("Server Stopped!")
+		fmt.Scan(&a)
+	}()
 	http.HandleFunc("/", IndexHandler)
 	m := Handler{}
 	m.Start("downloads")
@@ -29,8 +37,6 @@ func main() {
 	m.Start("marketplace")
 	m.Start("devs")
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
-	fmt.Println("Server Started!" + port)
+	//fmt.Println("Server Started!" + port)
 	http.ListenAndServe(":"+port, nil)
-	fmt.Println("Server Stopped!")
-	fmt.Scan(&a)
 }
